98c074657e4e4ef1985556edd2011f15387038b2
Connor O'Brien <connor@overcasthq.com>
2020-03-30 15:43:07 +0100

    No Profanity Gem

---
+++ lib/no_profanity.rb
@@ -0,0 +1,20 @@
+class NoProfanity
+    def self.check(word)
+        banned_words = {
+	    'darn' => '****',
+	    'hell' => '****',
+	    'heck' => '****',
+            'hate' => '****',
+	    'simp' => '****',
+            'noob' => '****',
+            'feeder' => '******',
+            'bad' => '***',
+        }
+	new_word = word.downcase	
+	banned_words.each do |profanity, clean|
+	   word.gsub!(profranity, clean)
+	end
+	puts new_word
+	return new_word
+    end
+end
---
+++ no_profanity.gemspec
@@ -0,0 +1,9 @@
+Gem::Specification.new do |spec|
+    spec.name = 'no_profanity'
+    spec.version = '0.0.1'
+    spec.date = '2020-03-24'
+    spec.authors = ["Connor O'Brien"]
+    spec.summary = "block swear words"
+    spec.files = ["lib/no_profanity.rb"]
+    spec.require_paths = ["lib"] 
+end
