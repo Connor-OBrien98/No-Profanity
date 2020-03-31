122aea99d4503815a4a1812b6ee73fe2f812d9ef
Connor O'Brien <connor@overcasthq.com>
2020-03-31 17:57:01 +0100

    comments

--- lib/no_profanity.rb
+++ lib/no_profanity.rb
@@ -1,5 +1,6 @@
 class NoProfanity
     def self.check(word)
+    	# checks the inputted word, to see if it matches with any of these
         banned_words = {
 	    'darn' => '****',
 	    'hell' => '****',
@@ -10,10 +11,15 @@ class NoProfanity
             'feeder' => '******',
             'bad' => '***',
         }
-	new_word = word.downcase	
+    # converts the word to lowercase to avoid capital letters ignoring the program
+	new_word = word.downcase
+
+	# Uses gsub to loop through each forbidden word to check if there's a match	
 	banned_words.each do |profanity, clean|
 	  new_word.gsub!(profanity, clean)
 	end
+
+	# sets and returns the new word
 	puts new_word
 	return new_word
     end
